40a9ef9a8a223f094764e03436ef2ca3e39e2946
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2026-01-21 23:11:46 +0200

    ...

--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -8,41 +8,43 @@ export function Hero() {
   return (
     <>
       <section className="hero">
-        <div className="tag_">
-          <div className="icon">
-            <PiStarFourFill />
+        <div className="content">
+          <div className="tag_">
+            <div className="icon">
+              <PiStarFourFill />
+            </div>
+            <p>Melhore seu README.md</p>
           </div>
-          <p>Melhore seu README.md</p>
-        </div>
-        <h1>Criador de Readme</h1>
-        <p className="text">O Nice Readme é uma ferramenta intuitiva para criar e personalizar estatísticas do GitHub directamente no seu README. Oferece suporte a cartões de sequência, temas, cores, idiomas e pré-visualização em tempo real, tudo numa interface simples e visual. Ideal para developers que querem um perfil mais atrativo e profissional.</p>
-        <div className="btn">
-          <Botao
-            className="btn-geral txunar"
-            href="/streak-stats"
-            content={
-              <>
-                Txunar meu readme
-                <div className="icon">
-                  <HiOutlineArrowSmRight />
-                </div>
-              </>
-            }
-          />
+          <h1>Criador de Readme</h1>
+          <p className="text">O Nice Readme é uma ferramenta para criar e personalizar estatísticas do GitHub. Ideal para developers que querem um perfil mais organizado. Eu criei esse projecto para ajudar quem esta iniciando no GitHub, quem tem dificuldades de entender documentações mas gostaria de deixar seu README mais limpo, organizado e profissional, sem precisar escrever código.</p>
+          <div className="btn">
+            <Botao
+              className="btn-geral txunar"
+              href="/streak-stats"
+              content={
+                <>
+                  Txunar meu readme
+                  <div className="icon">
+                    <HiOutlineArrowSmRight />
+                  </div>
+                </>
+              }
+            />
 
-          <Botao
-            className="github"
-            href="https://github.com/heliocarlitos/nice-readme"
-            target="_blank"
-            content={
-              <>
-                <div className="icon">
-                  <FaGithub />
-                </div>
-                Github
-              </>
-            }
-          />
+            <Botao
+              className="github"
+              href="https://github.com/heliocarlitos/nice-readme"
+              target="_blank"
+              content={
+                <>
+                  <div className="icon">
+                    <FaGithub />
+                  </div>
+                  Github
+                </>
+              }
+            />
+          </div>
         </div>
       </section>
     </>
