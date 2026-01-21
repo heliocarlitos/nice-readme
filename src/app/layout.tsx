ecefd72b15298354fac9676f41be385f31824ab9
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2026-01-21 23:46:41 +0200

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import { Header } from "@/components/header/header"
 import "./globals.css"
 import { LenisWrapper } from "@/components/leniswrapper/page"
+import TopProgressLoader from "@/components/topprogressloader/TopProgressLoader"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt">
       <body>
+        <TopProgressLoader />
         <Header />
         <LenisWrapper>
           <main>{children}</main>
